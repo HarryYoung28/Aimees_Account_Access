58df18e597a140944024f9b80b50447a81b5ad24
HarryYoung <harryyoung1995@icloud.com>
2024-06-18 21:04:20 +0100

    updated frames and name change to Hacktastic HQ

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ root = Tk()
 # define size
 root.minsize(width=1400, height=800)
 # create Title
-root.title("Account Access Manager")
+root.title("Hacktastic HQ - Hack That Account!")
 # prevent accidental resizing of window
 root.resizable(width=False, height=False)
 
@@ -24,29 +24,45 @@ for row in range(3):
 root.grid_rowconfigure(index=3, minsize=500)
 
 # title label on screen
-title_label = Label(root, text="Account Access Manager", font=("Helvetica", 30))
+title_label = Label(root, text="Hacktastic HQ", font=("Helvetica", 40))
 title_label.grid(row=0, column=0, columnspan=2)
 
 # buttons for quick links made into frame
 quick_links_frame = Frame(root, width=1400, height=100)
 quick_links_frame.grid(row=1, column=0, columnspan=2)
 # home button
-home_button = Button(quick_links_frame, text="HOME", font=("Helvetica", 24))
+home_button = Button(quick_links_frame, text="HOME", font=("Helvetica", 30))
 home_button.grid(row=1, column=0)
 
 # access account button
-access_account_button = Button(quick_links_frame, text="ACCESS ACCOUNT", font=("Helvetica", 24))
+access_account_button = Button(quick_links_frame, text="ACCESS ACCOUNT", font=("Helvetica", 30))
 access_account_button.grid(row=1, column=1, padx=250)
+
 # about button
-about_button = Button(quick_links_frame, text="ABOUT", font=("Helvetica", 24))
+about_button = Button(quick_links_frame, text="ABOUT", font=("Helvetica", 30))
 about_button.grid(row=1, column=3)
 
-# labels for how to use and user reviews
-how_to_use_label = Label(root, text="How to use our system", font=("Helvetica", 24))
-how_to_use_label.grid(row=2, column=0)
+# frames to differentiate how to use and user reviews
+how_to_use_label_frame = Frame(root, width=700, height=100)
+# border width and relief to create a black edging
+how_to_use_label_frame.configure(borderwidth=2, relief=SOLID)
+# sticky north, south, east, west to ensure it hits all sides
+how_to_use_label_frame.grid(row=2, column=0, sticky=NSEW)
 
-user_reviews_label = Label(root, text="User reviews", font=("Helvetica", 24))
-user_reviews_label.grid(row=2, column=1)
+user_reviews_label_frame = Frame(root, width=700, height=100)
+# border width and relief to create a black edging
+user_reviews_label_frame.configure(borderwidth=2, relief=SOLID)
+# sticky north, south, east, west to ensure it hits all sides
+user_reviews_label_frame.grid(row=2, column=1, sticky=NSEW)
+
+# labels for how to use and user reviews
+how_to_use_label = Label(how_to_use_label_frame, text="How To Use Our System", font=("Helvetica", 30))
+# expand and fill both to ensure label is in the middle of the frame
+how_to_use_label.pack(expand=True, fill=BOTH)
+
+user_reviews_label = Label(user_reviews_label_frame, text="User Reviews", font=("Helvetica", 30))
+# expand and fill both to ensure label is in the middle of the frame
+user_reviews_label.pack(expand=True, fill=BOTH)
 
 # mainloop
 root.mainloop()
