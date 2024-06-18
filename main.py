0c54a8e33c1f36b2c3889246d0fd0e29078d50fb
HarryYoung <harryyoung1995@icloud.com>
2024-06-18 21:36:02 +0100

    more frames added bro

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ root.resizable(width=False, height=False)
 # loop for column to be 700 wide each
 for column in range(2):
     root.grid_columnconfigure(index=column, minsize=700)
-    print(column)
 
 # loop for first 3 rows to be 100 height
 for row in range(3):
@@ -42,16 +41,16 @@ access_account_button.grid(row=1, column=1, padx=250)
 about_button = Button(quick_links_frame, text="ABOUT", font=("Helvetica", 30))
 about_button.grid(row=1, column=3)
 
-# frames to differentiate how to use and user reviews
+# frames to differentiate how to use label and user reviews label
 how_to_use_label_frame = Frame(root, width=700, height=100)
 # border width and relief to create a black edging
-how_to_use_label_frame.configure(borderwidth=2, relief=SOLID)
+how_to_use_label_frame.configure(borderwidth=2, relief=GROOVE)
 # sticky north, south, east, west to ensure it hits all sides
 how_to_use_label_frame.grid(row=2, column=0, sticky=NSEW)
 
 user_reviews_label_frame = Frame(root, width=700, height=100)
 # border width and relief to create a black edging
-user_reviews_label_frame.configure(borderwidth=2, relief=SOLID)
+user_reviews_label_frame.configure(borderwidth=2, relief=GROOVE)
 # sticky north, south, east, west to ensure it hits all sides
 user_reviews_label_frame.grid(row=2, column=1, sticky=NSEW)
 
@@ -63,6 +62,15 @@ how_to_use_label.pack(expand=True, fill=BOTH)
 user_reviews_label = Label(user_reviews_label_frame, text="User Reviews", font=("Helvetica", 30))
 # expand and fill both to ensure label is in the middle of the frame
 user_reviews_label.pack(expand=True, fill=BOTH)
+
+# frames for how to use instructions and user reviews content
+how_to_use_instructions_frame = Frame(root, width=700, height=500)
+how_to_use_instructions_frame.configure(borderwidth=2, relief=GROOVE)
+how_to_use_instructions_frame.grid(row=3, column=0, sticky=NSEW)
+
+user_reviews_content_frame = Frame(root, width=700, height=500)
+user_reviews_content_frame.configure(borderwidth=2, relief=GROOVE)
+user_reviews_content_frame.grid(row=3, column=1, sticky=NSEW)
 
 # mainloop
 root.mainloop()
